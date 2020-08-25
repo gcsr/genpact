e602b52e330dd0e1e0b8c55c5e273d98e26dfe9b
gcsr <gc.sekhar002@gmail.com>
2020-08-25 13:56:44 +0530

    mockmvc test case added

--- src/main/java/genpact/hackthon/interview/entity/Book.java
+++ src/main/java/genpact/hackthon/interview/entity/Book.java
@@ -5,11 +5,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class Book {
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;	
+	private String name;
+	private int quantity;
 
 }
---
+++ src/test/java/genpact/hackthon/interview/controller/BookControllerTest.java
@@ -0,0 +1,101 @@
+package genpact.hackthon.interview.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import genpact.hackthon.interview.entity.Book;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+
+	private static final Logger logger = LoggerFactory.getLogger(BookControllerTest.class);
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void add()  {
+		Book book = new Book();
+		book.setName("Book 1");
+		book.setQuantity(10);
+		try {
+			this.mockMvc
+					.perform(MockMvcRequestBuilders.post("/book").content(asJsonString(book))
+							.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isCreated()).andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.error("Test Case Failed at add");
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void getAll() {
+
+		try {
+			add();
+			this.mockMvc.perform(MockMvcRequestBuilders.get("/book").accept(MediaType.APPLICATION_JSON)).andDo(print())
+					.andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+					.andExpect(MockMvcResultMatchers.jsonPath("$.books[*].id").isNotEmpty());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.error("Test Case Failed at getAll");
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void getById() {
+
+		try {
+			add();
+			this.mockMvc.perform(MockMvcRequestBuilders.get("/book/1").accept(MediaType.APPLICATION_JSON)).andDo(print())
+					.andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+					.andExpect(MockMvcResultMatchers.jsonPath("$.books[*].id").isNotEmpty());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.error("Test Case Failed at getAll");
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void deleteId() {
+
+		try {
+			add();
+			this.mockMvc.perform(MockMvcRequestBuilders.delete("/book/1").accept(MediaType.APPLICATION_JSON)).andDo(print())
+					.andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+					.andExpect(MockMvcResultMatchers.jsonPath("$.books[*].id").isNotEmpty());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.error("Test Case Failed at getAll");
+			e.printStackTrace();
+		}
+	}
+
+
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
